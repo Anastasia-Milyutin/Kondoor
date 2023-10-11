5a7d9d87d8525af1ecf22d49591d3e5eb152c014
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-10-11 18:34:04 +0600

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ document.querySelector(".question__btn").addEventListener("click", function () {
     answerBlock.style.display = "none";
   }
 });
+
+const menuIcone = document.querySelector(".menu-icon"),
+  header = document.querySelector(".header__menu-link__flex");
+
+menuIcone.addEventListener("click", () => {
+  menuIcone.classList.toggle("menu-icon-active");
+  header.classList.toggle("header__active");
+});
